6683f1a8b91e787aae656ecef74fbe7b95f42b74
MD Mursedul Islam Sumon <sumon766@gmail.com>
2023-06-16 17:06:27 +0600

    Fix solver

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,43 +3,43 @@ require_relative '../solver'
 RSpec.describe Solver do
   describe '#factorial' do
     it 'returns 1 for 0' do
-      expect(Solver.new.factorial(0)).to eq(1)
+      expect(subject.factorial(0)).to eq(1)
     end
 
     it 'returns 1 for 1' do
-      expect(Solver.new.factorial(1)).to eq(1)
+      expect(subject.factorial(1)).to eq(1)
     end
 
     it 'raises an error for negative numbers' do
-      expect { Solver.new.factorial(-1) }.to raise_error(ArgumentError)
+      expect { subject.factorial(-1) }.to raise_error(ArgumentError)
     end
 
     it 'returns the correct factorial for positive numbers' do
-      expect(Solver.new.factorial(7)).to eq(5040)
+      expect(subject.factorial(7)).to eq(5040)
     end
   end
 
   describe '#reverse' do
     it 'reverses a string' do
-      expect(Solver.new.reverse('hello')).to eq('olleh')
+      expect(subject.reverse('hello')).to eq('olleh')
     end
   end
 
   describe '#fizzbuzz' do
     it 'returns "fizz" for multiples of 3' do
-      expect(Solver.new.fizzbuzz(9)).to eq('fizz')
+      expect(subject.fizzbuzz(9)).to eq('fizz')
     end
 
     it 'returns "buzz" for multiples of 5' do
-      expect(Solver.new.fizzbuzz(10)).to eq('buzz')
+      expect(subject.fizzbuzz(10)).to eq('buzz')
     end
 
     it 'returns "fizzbuzz" for multiples of 15' do
-      expect(Solver.new.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
     end
 
     it 'returns the input number for other numbers' do
-      expect(Solver.new.fizzbuzz(20)).to eq(20)
+      expect(subject.fizzbuzz(20)).to eq(20)
     end
   end
 end
